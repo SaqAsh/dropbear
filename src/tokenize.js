9436ba12c5de5f50af8f979537bf9d00d53b3f3b
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2025-07-30 22:16:46 -0400

    tokenize done

--- src/tokenize.js
+++ src/tokenize.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const {
   isLetter,
   isWhitespace,
@@ -6,6 +8,70 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+/**
+ * @param {string} input - The string to tokenize
+ * @returns {{ type: string, value: string | number }[]} An array of token objects.
+ **/
+const tokenize = (input) => {
+  const tokens = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    const character = input[cursor];
+
+    if (isParenthesis(character)) {
+      tokens.push({
+        type: 'Parenthesis',
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(character)) {
+      let number = character;
+
+      while (isNumber(input[++cursor])) {
+        number += input[cursor];
+      }
+
+      tokens.push({ type: 'Number', value: parseInt(number, 10) });
+      continue;
+    }
+
+    if (isLetter(character)) {
+      let identifier = character;
+
+      while (isLetter(input[++cursor])) {
+        identifier += input[cursor];
+      }
+
+      tokens.push({ type: 'Name', value: identifier });
+      continue;
+    }
+
+    if (isQuote(character)) {
+      let string = '';
+
+      while (!isQuote(input[++cursor])) {
+        string += input[cursor];
+      }
+
+      tokens.push({ type: 'String', value: string });
+
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`Invalid input: ${character}`);
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
