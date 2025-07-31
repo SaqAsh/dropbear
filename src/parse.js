c21ddf91980028f24e00ec9759d6c4a77b951e88
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2025-07-30 23:37:48 -0400

    parser done

--- src/parse.js
+++ src/parse.js
@@ -2,8 +2,64 @@ const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
-const parenthesize = () => {};
+/**
+ * @param
+ * @returns
+ **/
+const parenthesize = (tokens) => {
+  const token = pop(tokens);
 
-const parse = () => {};
+  if (isOpeningParenthesis(token.value)) {
+    const expression = [];
+    while (!isClosingParenthesis(peek(tokens).value)) {
+      expression.push(parenthesize(tokens));
+    }
 
-module.exports = { parse: tokens => parse(parenthesize(tokens)) };
+    pop(tokens);
+    return expression;
+  }
+
+  // we have hit the leaf node
+  return token;
+};
+
+/**
+ * @param {any[]}
+ * @returns
+ **/
+const parse = (tokens) => {
+  // we need to call the same function over again for each value, until we have only one value, which then we can identify
+  if (Array.isArray(tokens)) {
+    const [first, ...rest] = tokens;
+    return {
+      type: 'CallExpression',
+      name: first.value,
+      arguments: rest.map(parse),
+    };
+  }
+
+  const token = tokens;
+
+  if (token.type === 'Number') {
+    return {
+      type: 'NumericLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'String') {
+    return {
+      type: 'StringLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'Name') {
+    return {
+      type: 'Identifier',
+      name: token.value,
+    };
+  }
+};
+
+module.exports = { parse: (tokens) => parse(parenthesize(tokens)) };
